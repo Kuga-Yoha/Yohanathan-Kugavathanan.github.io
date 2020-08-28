755bc3a435f008080196439b6e22842808f3361c
Yohanathan-Kugavathanan <kugajpx2012@gmail.com>
2020-08-28 19:42:00 +0530

    Updates

---
+++ precache-manifest.8d9e36f4cc970b2740485945e319ed6b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30f731add6ba01b63fcc9f2ceb20b16",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "c1bd7f71589c3fb5a7eb",
+    "url": "/cv/static/css/main.61052cfd.chunk.css"
+  },
+  {
+    "revision": "1558c68c8105066bd9a9",
+    "url": "/cv/static/js/2.6bc79447.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv/static/js/2.6bc79447.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1bd7f71589c3fb5a7eb",
+    "url": "/cv/static/js/main.b4360b3b.chunk.js"
+  },
+  {
+    "revision": "9e0e921495960402e4a4",
+    "url": "/cv/static/js/runtime-main.c17789b0.js"
+  },
+  {
+    "revision": "070a9269082474ecd06b6d64ebd0fa2f",
+    "url": "/cv/static/media/GlacialIndifference-Bold.070a9269.otf"
+  },
+  {
+    "revision": "008080d5594fd00507fc8a2c93443d39",
+    "url": "/cv/static/media/GlacialIndifference-Regular.008080d5.otf"
+  },
+  {
+    "revision": "7bf226306214181d5e917ed5bda2301c",
+    "url": "/cv/static/media/norwester.7bf22630.otf"
+  }
+]);
